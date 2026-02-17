03e50560949d67b0f86f753ea006105846c3fa35
everybodysimulator <ga1010@wscloud.org>
2026-02-17 16:52:13 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CHANGE THIS every time you update site files
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 
 // Unique cache name
 const CACHE_NAME = 'nautilus-cache-' + CACHE_VERSION;
